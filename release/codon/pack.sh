ecd311ae9d35b41397574dbd03d7cf066b33c07c
Marcel Kottmann <marcel.kottmann.hh@gmail.com>
2025-11-16 13:24:36 +0100

    feat: upload artifacts

---
+++ release/codon/pack.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p release/armembeddon/bin
+mkdir -p release/armembeddon/lib
+
+cp llvm-project/install/bin/llc release/armembeddon/bin/llc
+cp llvm-project/install/bin/llvm-objcopy release/armembeddon/bin/llvm-objcopy
+
+cp codon/install/bin/codon release/armembeddon/bin/codon
+cp -r codon/install/lib/codon/ release/armembeddon/lib/
+
+tar cvzf armembeddon.tar.gz -C release armembeddon
