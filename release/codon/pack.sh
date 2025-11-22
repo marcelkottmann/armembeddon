fffeb92707dea381cdfc378d1bcf3b929114213b
Marcel Kottmann <marcel.kottmann.hh@gmail.com>
2025-11-22 08:25:56 +0100

    feat: use gh for auth

--- release/codon/pack.sh
+++ release/codon/pack.sh
@@ -12,3 +12,6 @@ cp codon/install/bin/codon release/armembeddon/bin/codon
 cp -r codon/install/lib/codon/ release/armembeddon/lib/
 
 tar cvzf armembeddon.tar.gz -C release armembeddon
+
+gh --version
+gh api user
