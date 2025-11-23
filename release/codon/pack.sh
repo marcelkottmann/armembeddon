fde7b2f17203ca506b626d32d4b1e19c4b0930e4
Marcel Kottmann <marcel.kottmann.hh@gmail.com>
2025-11-23 12:12:51 +0100

    feat: create gh release

--- release/codon/pack.sh
+++ release/codon/pack.sh
@@ -5,13 +5,17 @@ set -e
 mkdir -p release/armembeddon/bin
 mkdir -p release/armembeddon/lib
 
+# copy llvm binaries
 cp llvm-project/install/bin/llc release/armembeddon/bin/llc
 cp llvm-project/install/bin/llvm-objcopy release/armembeddon/bin/llvm-objcopy
 
+# copy codon libs and binaries
 cp codon/install/bin/codon release/armembeddon/bin/codon
 cp -r codon/install/lib/codon/ release/armembeddon/lib/
 
+# copy armembeddon files
+cp -r armembeddon/* release/armembeddon/
+
 tar cvzf armembeddon.tar.gz -C release armembeddon
 
-gh --version
-gh api user
+gh release create "$VERSION" --repo marcelkottmann/armembeddon ./armembeddon.tar.gz
